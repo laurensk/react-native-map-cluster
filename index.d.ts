f5417535f33cef488784454deeb31528096e862d
Laurens K <39766854+laurensk@users.noreply.github.com>
2021-01-13 00:19:15 +0100

    fixed types

--- index.d.ts
+++ index.d.ts
@@ -2,14 +2,14 @@ import React from "react";
 
 interface ClusteredMapViewProps {
   // general props
-  style: object | null;
-  data: any[];
-  initialRegion: object;
-  ref: Function | null;
-  renderMarker: Function;
-  renderCluster: Function;
-  showsUserLocation: boolean | null;
-  permissionsAndroid: {
+  style?: object | null;
+  data?: any[];
+  initialRegion?: object;
+  ref?: Function | null;
+  renderMarker?: Function;
+  renderCluster?: Function;
+  showsUserLocation?: boolean | null;
+  permissionsAndroid?: {
     title: string;
     message: string;
     buttonNeutral: string;
@@ -18,22 +18,26 @@ interface ClusteredMapViewProps {
   } | null;
 
   // additional props
-  radius: number | null;
-  extent: number | null;
-  minZoom: number | null;
-  maxZoom: number | null;
-  width: number | null;
-  height: number | null;
-  onExplode: Function | null;
-  onImplode: Function | null;
-  onClusterPress: Function | null;
-  preserveClusterPressBehavior: boolean | null;
-  clusterPressMaxChildren: Function | null;
-  edgePadding: object | null;
-  animateClusters: boolean | null;
-  layoutAnimationConf: any | null;
-  clusteringEnabled: boolean | null;
-  accessor: String | Function | null;
+  radius?: number | null;
+  extent?: number | null;
+  minZoom?: number | null;
+  maxZoom?: number | null;
+  width?: number | null;
+  height?: number | null;
+  onExplode?: Function | null;
+  onImplode?: Function | null;
+  onClusterPress?: Function | null;
+  preserveClusterPressBehavior?: boolean | null;
+  clusterPressMaxChildren?: Function | null;
+  edgePadding?: object | null;
+  animateClusters?: boolean | null;
+  layoutAnimationConf?: any | null;
+  clusteringEnabled?: boolean | null;
+  accessor?: String | Function | null;
 }
 
-export class ClusteredMapView extends React.Component<ClusteredMapViewProps, any> {}
+declare class ClusteredMapView extends React.Component<ClusteredMapViewProps> {
+  constructor(props: ClusteredMapViewProps);
+}
+
+export default ClusteredMapView;
